8e8ded8b3c5ee14f4ee4d028e496be971619b62d
Victor <grester43hck@gmail.com>
2017-04-23 20:15:36 +0200

    mongo driver tests

--- api/helpers/multiusos.php
+++ api/helpers/multiusos.php
@@ -9,6 +9,16 @@
 include_once "../header.php";
 
 
-$client = new MongoConector();
-$res = $client->find(new testModel());
-var_dump($res);
+require_once '../Google/autoload.php';
+
+// Get $id_token via HTTPS POST.
+
+$client = new Google_Client(['client_id' => "440760096024-d7ai1dsjv219r8rb97e0iusts6hqca9s.apps.googleusercontent.com"]);
+$payload = $client->verifyIdToken($id_token);
+if ($payload) {
+    $userid = $payload['sub'];
+    // If request specified a G Suite domain:
+    //$domain = $payload['hd'];
+} else {
+    // Invalid ID token
+}
