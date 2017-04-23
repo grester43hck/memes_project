77e9c7532c2bf044145ab383acb5d71b96dce0e2
Victor <grester43hck@gmail.com>
2017-04-23 20:04:41 +0200

    mongo driver tests

---
+++ api/Controllers/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 23/04/17
+ * Time: 19:55
+ */
+abstract class Controller
+{
+
+
+
+}
---
+++ api/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 23/04/17
+ * Time: 20:03
+ */
+echo "index!";
+var_dump($_GET);
