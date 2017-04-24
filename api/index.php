7c66d21f8e5e97afbb5235b005f541c156f8200a
Victor <grester43hck@gmail.com>
2017-04-24 11:23:30 +0200

    framework test

--- api/index.php
+++ api/index.php
@@ -8,8 +8,8 @@
 
 include_once "header.php";
 
-$controller = (isset($_GET["c"]))?ucfirst($_GET["c"])."Controller":"ErrorController";
-$action = (isset($_GET["a"]))?ucfirst($_GET["a"])."Action":"MainAction";
+$controller = (isset($_GET["c"])&&$_GET["c"]!="")?ucfirst($_GET["c"])."Controller":"ErrorController";
+$action = (isset($_GET["a"])&&$_GET["a"]!="")?ucfirst($_GET["a"])."Action":"MainAction";
 var_dump($controller, $action);
 try {
     $controller::$action();
