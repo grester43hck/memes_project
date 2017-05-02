d1bfd72092b2ff52ceaf434863d1e1561a5ceb68
Victor <grester43hck@gmail.com>
2017-05-02 18:11:27 +0200

    auth 0.2

--- api/Controllers/UserController.php
+++ api/Controllers/UserController.php
@@ -15,4 +15,12 @@ class UserController extends BaseController
         echo "MAIN ACTION OF USER CONTROLLER";
     }
 
+    public static function login(){
+        $token = $_POST["token"];
+        $type = $_POST["type"];
+        $auth = new Authenticate();
+        $res = $auth->auth($token, $type);
+        parent::printJSON(array("success"=>($res)?true:false));
+    }
+
 }
--- api/index.php
+++ api/index.php
@@ -12,7 +12,8 @@ include_once "header.php";
 $token="";
 $type="google";
 $auth = new Authenticate();
-$auth->auth($tokem, $type);
+$user_id = $auth->auth($token, $type);
+if($user_id) define("USER", $user_id);
 
 $controller = (isset($_GET["c"])&&$_GET["c"]!="")?ucfirst($_GET["c"])."Controller":"ErrorController";
 $action = (isset($_GET["a"])&&$_GET["a"]!="")?ucfirst($_GET["a"])."Action":"MainAction";
