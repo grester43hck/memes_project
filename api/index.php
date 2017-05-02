f9415047c5618d78be848370068b43fbae21e67e
Victor <grester43hck@gmail.com>
2017-05-02 18:02:58 +0200

    auth 0.1

--- api/index.php
+++ api/index.php
@@ -8,6 +8,12 @@
 
 include_once "header.php";
 
+//AUTH
+$token="";
+$type="google";
+$auth = new Authenticate();
+$auth->auth($tokem, $type);
+
 $controller = (isset($_GET["c"])&&$_GET["c"]!="")?ucfirst($_GET["c"])."Controller":"ErrorController";
 $action = (isset($_GET["a"])&&$_GET["a"]!="")?ucfirst($_GET["a"])."Action":"MainAction";
 
