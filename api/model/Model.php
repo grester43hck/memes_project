58d966ee5f35f730ff8628fc184f301693ee2fa9
Victor <grester43hck@gmail.com>
2017-05-02 19:38:47 +0200

    auth 0.3

--- api/model/Model.php
+++ api/model/Model.php
@@ -19,7 +19,6 @@ abstract class Model
 
         $object = (array)$object;
         foreach(get_class_vars(get_class($this)) as $field=>$val){
-            var_dump($field);
             if($field=="id"){
                 $this->id = $object["_id"]->{'$id'};
             }else {
